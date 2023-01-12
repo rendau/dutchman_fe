dac3d048c3f11214d135d11cc10968cd011eaa6b
mdb <dauren.asir@gmail.com>
2023-01-12 12:01:42 +0600

    some changes

--- src/components/acs/InputStringArray.vue
+++ src/components/acs/InputStringArray.vue
@@ -1,7 +1,12 @@
 <template>
-  <q-input dense outlined>
-    hello
-  </q-input>
+  <div class="rounded-borders br2 q-pa-sm" style="min-height: 40px;">
+    <div class="row items-center q-gutter-y-xs q-gutter-x-sm">
+      <div>asd</div>
+    </div>
+  </div>
+  <!--  <q-input dense outlined>-->
+  <!--    hello-->
+  <!--  </q-input>-->
 </template>
 
 <script setup>
