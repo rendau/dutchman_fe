5f13d47063c852ef8cfe8a0dbd95b60f4dcd21ea
mdb <dauren.asir@gmail.com>
2023-01-16 12:25:46 +0600

    some changes

---
+++ src/layouts/AppList.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-list>
+    <q-item-label header>
+      <div class="row no-wrap items-center">
+        <div class="col">
+          Applications
+        </div>
+
+        <div class="q-pl-sm">
+          <q-btn dense flat round icon="add" color="primary" size="0.7rem" @click="onAddClick"/>
+        </div>
+      </div>
+    </q-item-label>
+
+    <q-item v-for="app in realmApps" :key="`menu-${app.name}`" dense class="q-pl-md"
+            active-class="bg-primary text-white">
+      <q-item-section side style="color: inherit">
+        <q-icon name="lan" size="1.1rem"/>
+      </q-item-section>
+
+      <q-item-section class="q-py-xs q-pl-xs">
+        {{ app.name }}
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+
+const store = useStore()
+const router = useRouter()
+
+const realm = computed(() => store.getters['data/selectedRealm'])
+const realmApps = computed(() => store.getters['data/selectedRealmApps'])
+
+const onAddClick = () => {
+  router.push({ name: 'app-create' })
+}
+</script>
