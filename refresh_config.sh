76c975562318dc32ad9ec6e5ebffa5d54dc9b7b3
mdb <dauren.asir@gmail.com>
2023-02-02 19:13:29 +0600

    some changes

--- refresh_config.sh
+++ refresh_config.sh
@@ -9,5 +9,5 @@ touch ${confpath}
 # Add assignment
 echo "window.API_URL = '$API_URL';" >> ${confpath}
 echo "window.ACCOUNT_API_URL = '$ACCOUNT_API_URL';" >> ${confpath}
-echo "window.ACCOUNT_AUTH_URL = '$ACCOUNT_API_URL';" >> ${confpath}
+echo "window.ACCOUNT_AUTH_URL = '$ACCOUNT_AUTH_URL';" >> ${confpath}
 echo "window.ACCOUNT_CABINET_URL = '$ACCOUNT_CABINET_URL';" >> ${confpath}
