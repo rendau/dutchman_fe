a0ad354840de74b51a729867e613eddea534f13c
mdb <dauren.asir@gmail.com>
2023-03-04 17:40:31 +0600

    some changes

--- src/components/endpoint/List.vue
+++ src/components/endpoint/List.vue
@@ -13,36 +13,49 @@
     </div>
 
     <!-- table -->
-    <q-markup-table flat bordered wrap-cells dense class="relative-position">
-      <tbody>
-      <tr v-for="(item, itemI) in items" :key="`item-${itemI}-${item.path}`"
-          class="cursor-pointer" :class="{'text-grey-6': !item.active}"
-          @click="onItemClick(item)">
-        <td class="text-right text-no-wrap text-weight-light min-width">
-          {{ item.method }}
-        </td>
 
-        <td class="text-no-wrap text-subtitle2">
-          /{{ item.path }}
-        </td>
+    <div class="column no-wrap items-stretch q-gutter-y-md">
+      <div v-for="grp in groupedItems" :key="`grp-${grp.path}`">
+        <div class="text-subtitle1 text-weight-medium text-grey-8 q-pb-sm">
+          /{{ grp.path }}
+        </div>
 
-        <td class="text-center min-width">
-          <div class="row no-wrap items-center justify-end q-gutter-sm">
-            <span v-if="item.ip_validation?.enabled" class="text-caption text-bold text-blue-grey-4">IP</span>
+        <div class="q-pl-md">
+          <q-markup-table flat bordered wrap-cells dense class="relative-position">
+            <tbody>
+            <tr v-for="(item, itemI) in grp.items" :key="`item-${itemI}-${item.path}`"
+                class="cursor-pointer" :class="{'text-grey-6': !item.active}"
+                @click="onItemClick(item)">
+              <td class="text-right text-no-wrap text-weight-light min-width">
+                <div style="min-width: 56px">
+                  {{ item.method }}
+                </div>
+              </td>
 
-            <q-icon v-if="item.jwt_validation?.enabled" name="lock" color="blue-grey-4" size=".85rem"/>
-          </div>
-        </td>
-      </tr>
+              <td class="text-no-wrap text-subtitle2">
+                <div class="q-pl-sm">
+                  /{{ item.path }}
+                </div>
+              </td>
 
-      <tr v-if="!items.length">
-        <td colspan="100%">
-          <q-btn dense flat no-caps icon="add" label="add endpoint" color="primary"
-                 class="full-width" @click="onAddClick"/>
-        </td>
-      </tr>
-      </tbody>
-    </q-markup-table>
+              <td class="text-center min-width">
+                <div class="row no-wrap items-center justify-end q-gutter-sm">
+                  <span v-if="item.ip_validation?.enabled" class="text-caption text-bold text-blue-grey-4">IP</span>
+
+                  <q-icon v-if="item.jwt_validation?.enabled" name="lock" color="blue-grey-4" size=".85rem"/>
+                </div>
+              </td>
+            </tr>
+            </tbody>
+          </q-markup-table>
+        </div>
+      </div>
+
+      <div v-if="!items.length">
+        <q-btn dense flat no-caps icon="add" label="add endpoint" color="primary"
+               class="full-width" @click="onAddClick"/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -66,6 +79,11 @@ const items = computed(() => _.sortBy(
       'method',
     ],
 ))
+const groupedItems = computed(() => {
+  return _.sortBy(_.map(_.groupBy(items.value, x => _.head(_.split(x.path, '/'))), (v, k) => {
+    return { path: k, items: v }
+  }), 'path')
+})
 
 const onAddClick = () => {
   router.push({ name: 'endpoint-create' })
